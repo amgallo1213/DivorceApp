4f0cf558cfbd165670f741b14f845f1747ae7700
Andrea <90292543+amgallo1213@users.noreply.github.com>
2023-02-04 17:25:03 -0500

    adding more styling

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,11 +1,20 @@
-
+import Form from 'react-bootstrap/Form';
 
 
 const SignUp = () => {
     return ( 
         <div id="sign-up">
             <h4>Sign Up</h4>
-            
+            <Form>
+                <Form.Group className="mb-3" >
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" />
+                </Form.Group>
+                <Form.Group className="mb-3" >
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+            </Form>
         </div>
     );
 }
