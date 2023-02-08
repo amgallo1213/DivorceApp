8b02a8ead486eb733a51e2466a2c6a6df77cfcc8
Andrea <90292543+amgallo1213@users.noreply.github.com>
2023-02-07 23:17:07 -0500

    added css to sign in component

--- client/src/components/SignIn.js
+++ client/src/components/SignIn.js
@@ -5,16 +5,22 @@ import Button from "react-bootstrap/Button";
 const SignIn = () => {
     return ( 
         <div id="sign-in">
-            <h3>Sign In To Your Account</h3>
-            <Form.Group className="mb-3" >
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
-                </Form.Group>
+            <div id="left-sign-in">
+                <h3>Sign In To Your Account</h3>
                 <Form.Group className="mb-3" >
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
-                </Form.Group>
-                <Button type="submit" id="sign-up-btn">Submit</Button>
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                    </Form.Group>
+                    <Form.Group className="mb-3" >
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+                    <Button type="submit" id="sign-up-btn">Submit</Button>
+                    <p id="sign-in-paragraph">Thank you for choosing our website to assist you in your divorce proceeding.
+                    Please visit our FAQ page for any questions or problems with your login.</p>
+            </div>
+            <img src="https://images.pexels.com/photos/6669845/pexels-photo-6669845.jpeg?auto=compress&cs=tinysrgb&w=800" id="sign-in-image"></img>
+            
         </div>
     );
 }
