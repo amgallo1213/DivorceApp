7955048263f7ba0dbd6824c782022aad78f2b01d
Andrea <90292543+amgallo1213@users.noreply.github.com>
2023-02-07 23:23:37 -0500

    updating css on sign in componenet

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,7 +1,7 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import  Button  from 'react-bootstrap/Button';
+
 
 
 const NavBar = () => {
@@ -11,9 +11,10 @@ const NavBar = () => {
                 <Container>
                 <Navbar.Brand href="#home" class="navbar">Pro Se Divorce</Navbar.Brand>
                 <Nav className="me-auto">
-                    <Nav.Link href="#home" class="navbar">Home</Nav.Link>
-                    <Nav.Link href="#features" class="navbar">Features</Nav.Link>
-                    <Nav.Link href="#pricing" class="navbar">Pricing</Nav.Link>
+                    <Nav.Link href="#home" class="navbar nav-bar">Home</Nav.Link>
+                    <Nav.Link href="#features" class="navbar nav-bar">Features</Nav.Link>
+                    <Nav.Link href="#pricing" class="navbar nav-bar">Pricing</Nav.Link>
+                    <Nav.Link href="#faq" class="navbar nav-bar">FAQ</Nav.Link>
                     <Nav.Link href="signin">Sign In</Nav.Link>
                 </Nav>
                 </Container>
